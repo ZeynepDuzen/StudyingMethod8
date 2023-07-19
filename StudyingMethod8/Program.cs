e044c9181184b48870938935d7e884d123e27450
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-19 07:12:53 +0300

    Proje dosyası ekle.

---
+++ StudyingMethod8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyingMethod8
+{
+    internal class Program
+    {
+        static void Travel(string travel = " new country")
+        {
+            Console.WriteLine("to see a" + " " + travel);
+        }
+        static void Main(string[] args)
+        {
+            Travel();
+            Travel("beautiful sea");
+            Travel("cultural places");
+             
+            Console.Read();
+
+                }
+    }
+}
